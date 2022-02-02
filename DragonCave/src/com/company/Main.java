cf33f12a7ce8d166c5b2cc98400f3bc6dca40700
Iatzy <iatzy.graciani@genspark.net>
2022-02-01 23:17:28 -0400

    Try and catch
    Added a try & catch along with a while loop to verify correct user input

--- DragonCave/src/com/company/Main.java
+++ DragonCave/src/com/company/Main.java
@@ -1,19 +1,39 @@
 package com.company;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
         public static void main (String[]args)
         {
-            int input_num;
+            int input_num=0;
             int max=2;
             int  min=1;
             int random_num = (int)Math.floor(Math.random()*(max-min+1)+min);
+            boolean b = true;
             Scanner input = new Scanner(System.in);
             System.out.println("You are in the land full of dragons. In front of you, you see two caves.");
             System.out.println("In one cave, the dragon is friendly and will share his treasure with you.");
             System.out.println("The other dragon is greedy and hungry and will eat you on sight.");
             System.out.println("Which cave will you go into?(1 or 2)");
-            input_num= input.nextInt();
+            while(b){ // loop that contains a verification process and a try
+                // and catch exception handling for incorrect user input
+            try
+            {
+                input_num= input.nextInt();
+                if(input_num==1||input_num==2)
+                    b=false;
+                else
+                {
+                    System.out.println("Only the numbers 1 and 2 are allowed. Try Again");
+
+                }
+            }
+            catch(InputMismatchException ex){
+                System.out.println("Invalid input, enter 1 or 2");
+                input.next();
+
+
+            }}
             if(input_num==random_num) {
                 System.out.println("You approach the cave...");
                 System.out.println("It is dark and spooky...");
